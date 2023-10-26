5247fce06e9b107ea37c12f58d8f810391251ed3
KKKHun2 <1224kim2son@naver.com>
2023-10-26 19:32:23 +0900

    ✨중복제외,버튼으로 새로운번호출력

--- src/components/LottoNumberDraw.tsx
+++ src/components/LottoNumberDraw.tsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { lottoData } from '../data/data'
 
 const LottoNumberDraw: React.FC = () => {
-  const combineAndRecommendNumbers = () => {
-    const allNumbers = lottoData.flat();
+    const [recommendedNumbers, setRecommendedNumbers] = useState<number[]>([]);
 
-    const recommendedNumbers: number[] = [];
-
-    while (recommendedNumbers.length < 6) {
-      const randomIndex = Math.floor(Math.random() * allNumbers.length);
-      recommendedNumbers.push(allNumbers[randomIndex]);
-    }
-
-    return recommendedNumbers;
+    const generateRandomNumbers = () => {
+      const allNumbers = lottoData.flat();
+      const uniqueRandomNumbers: number[] = [];
+  
+      while (uniqueRandomNumbers.length < 6) {
+        const randomIndex = Math.floor(Math.random() * allNumbers.length);
+        const randomNum = allNumbers[randomIndex];
+  
+        if (!uniqueRandomNumbers.includes(randomNum)) {
+          uniqueRandomNumbers.push(randomNum);
+        }
+      }
+  
+      setRecommendedNumbers(uniqueRandomNumbers);
+    };
+  
+    return (
+      <div>
+        <h1>로또 번호 추천기</h1>
+        <button onClick={generateRandomNumbers}>번호 추천</button>
+        <p>추천 번호: {recommendedNumbers.join(', ')}</p>
+      </div>
+    );
   };
-
-  const recommendedNumbers = combineAndRecommendNumbers();
-
-  return (
-    <div>
-      <h1>로또 번호 추천기</h1>
-      <p>추천 번호: {recommendedNumbers.join(', ')}</p>
-    </div>
-  );
-};
 
 export default LottoNumberDraw;
