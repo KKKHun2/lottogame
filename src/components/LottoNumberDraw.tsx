51b7eaacc192ff51fecb2a84397b73bdd12d5d93
KKKHun2 <1224kim2son@naver.com>
2023-10-27 23:38:46 +0900

    ✨기본 디자인2차 수정

--- src/components/LottoNumberDraw.tsx
+++ src/components/LottoNumberDraw.tsx
@@ -1,42 +1,69 @@
 import React, { useState } from 'react';
 import { lottoData } from '../data/data';
 import styled from 'styled-components';
-import { BsCircleFill } from 'react-icons/bs';
 
 const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   text-align: center;
   background-color: #00a8ff;
   padding: 20px;
+  height: 100vh;
   border-radius: 10px;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 50px;
+  font-size: 60px;
+  font-weight: 700;
 `;
 
 const Button = styled.button`
   background-color: #f5f6fa;
   border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
+  padding: 15px 30px;
+  border-radius: 10px;
   font-size: 18px;
   cursor: pointer;
+  margin-bottom: 80px;
+  transition: all 0.3s ease; 
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+  
+  &:hover {
+    background-color: #a2a2a2;
+    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.4); 
+  }
 `;
+
 
 const RecommendedNumbersContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  margin-bottom: 300px;
 `;
 
 const RecommendedNumber = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 50px;
-  height: 50px;
+  width: 100px;
+  height: 100px;
   background-color: #f5f6fa;
   border: 2px solid #00a8ff;
   border-radius: 50%;
-  margin: 5px;
+  margin: 10px;
   font-size: 20px;
+  box-shadow:0 10px 10px rgba(0, 0, 0, 0.4); 
 `;
+
+
+const LottoN = styled.div`
+    font-size:35px;
+    font-weight: 450;
+`
 
 const LottoNumberDraw: React.FC = () => {
   const [recommendedNumbers, setRecommendedNumbers] = useState<number[]>([]);
@@ -60,13 +87,14 @@ const LottoNumberDraw: React.FC = () => {
 
   return (
     <Container>
-      <h1>로또 번호 추천기</h1>
+      <Title>로또 번호 추천기</Title>
       <Button onClick={generateRandomNumbers}>번호 추천</Button>
       <RecommendedNumbersContainer>
         {recommendedNumbers.map((number, index) => (
           <RecommendedNumber key={index}>
-            <BsCircleFill />
+            <LottoN>
             {number}
+            </LottoN>
           </RecommendedNumber>
         ))}
       </RecommendedNumbersContainer>
