ae49d6225bdcf308757e9176fef4b84d4313fc05
KKKHun2 <1224kim2son@naver.com>
2023-11-03 17:12:10 +0900

    ⚡매주마다 새롭게 로또번호 자동추가되게 설정중

--- src/components/AddLotto.ts
+++ src/components/AddLotto.ts
@@ -1,34 +1,41 @@
-export {AddLotto};
+import axios from 'axios';
+import fs from 'fs';
 
-const axios = require('axios');
-const fs = require('fs');
+let currentDrawNo: number = 1090;
+let lastExecutionDate: Date = new Date();
 
-const lottoDrawNo = 1090
+function checkAndAddLotto() {
+  const now: Date = new Date();
 
-const apiUrl = `http://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${lottoDrawNo}`;
+  if (now.getTime() - lastExecutionDate.getTime() >= 7 * 24 * 60 * 60 * 1000) {
+    AddLotto(currentDrawNo);
+    currentDrawNo++;
+    lastExecutionDate = now;
+  }
+}
 
-
-async function AddLotto() {
+async function AddLotto(drawNo: number) {
+  const apiUrl: string = `http://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${drawNo}`;
   try {
     const response = await axios.get(apiUrl);
-    const lottoData = response.data; 
+    const lottoData = response.data;
 
     const winningNumbers = [];
     for (let i = 1; i <= 6; i++) {
       winningNumbers.push(lottoData[`drwtNo${i}`]);
     }
 
-    const dataToAppend = `export const lottoData = [..., [${winningNumbers.join(', ')}]];\n`; // 기존 데이터에 winningNumbers 추가
-    fs.appendFile('data.ts', dataToAppend,(err: NodeJS.ErrnoException | null) => {
+    const dataToAppend = `export const lottoData = [..., [${winningNumbers.join(', ')}]];\n`;
+    fs.appendFile('data.ts', dataToAppend, (err) => {
       if (err) {
         console.error('데이터를 추가하는 중 오류 발생:', err);
       } else {
-        console.log('로또 번호 데이터가 추가되었습니다.');
+        console.log(`로또 번호 데이터가 추가되었습니다. (회차: ${drawNo})`);
       }
     });
   } catch (error) {
     console.error('API 호출 중 오류 발생:', error);
   }
 }
-
-AddLotto();
+setInterval(checkAndAddLotto, 60 * 60 * 1000);
+checkAndAddLotto();
